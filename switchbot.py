2c9158d0abd5755824074882bbd8e4efbff6fcbd
Dale Gundersen <d.gundersen@gmail.com>
2022-08-31 11:57:19 -0600

    Implemented recording temp/humidity to timestream

--- switchbot.py
+++ switchbot.py
@@ -1,6 +1,8 @@
 import sys
 from switchbot.switchbot_api import SwitchBotApi
 from switchbot.switchbot_devices import Meter
+from switchbot.config import AppConfig
+from timestream.write_client import WriteClient
 
 def test_devices(bot_press=False):
 
@@ -28,39 +30,102 @@ def desklight():
 
     switchbot.bot_press(switchbot.desklight_device_id)
 
-def print_meter_readings():
-    print('Getting temp/humidity..\n')
+# {'deviceId': 'FD184178F166', 'deviceName': 'Temp 1 Basement', 'deviceType': 'Meter', 'enableCloudService': True, 'hubDeviceId': 'E248AB2668E1'}
+def get_meters_from_api_devices():
 
     switchbot = SwitchBotApi()
 
-    # TODO: configure list of meter ids rather than fetching them every time
     devices = switchbot.get_devices()
 
     meters = []
 
     if devices:
         for device in devices:
-            print(device)
+            # print(device)
 
             if 'deviceType' in device and device['deviceType'] == 'Meter':
-                meter = Meter(name=device['deviceName'])
+                meter = Meter(name=device['deviceName'], device_id=device['deviceId'])
 
                 status = switchbot.get_device_status(device['deviceId'])
 
                 meter.set_measurements(temp=status['temperature'], humidity=status['humidity'])
 
                 meters.append(meter)
+
     else:
         print('No devices found')
+
+    return meters
+
+# Gets temp/humidity and prints to console
+def print_meter_readings():
+    print('Getting temp/humidity..\n')
+
+    meters = get_meters_from_api_devices()
 
     if meters:
         meters = sorted(meters, key=lambda x: x.device_name)
 
         for meter in meters:
             print(meter)
+
     else:
         print('No meters found')
 
+# Gets temp/humidity and stores in timestream
+def record_meter_readings():
+    print('Recording temp/humidity..')
+
+    meters = get_meters_from_api_devices()
+
+    if meters:
+
+        app_config = AppConfig()
+
+        aws_config = app_config.config['aws']
+
+        write_client = WriteClient(region="us-east-1", aws_config=aws_config)
+
+        current_time = write_client.current_milli_time()
+
+        records = []
+        for meter in meters:
+
+            dimensions = [
+                {'Name': 'deviceName', 'Value': meter.device_name},
+                {'Name': 'deviceType', 'Value': 'Meter'},
+                {'Name': 'deviceId', 'Value': meter.device_id}
+            ]
+
+            # All values have to be passed as strings, even if the value type is numerical
+            temp = {
+                'Dimensions': dimensions,
+                'MeasureName': 'temperature',
+                'MeasureValue': str(meter.temperature),
+                'MeasureValueType': 'DOUBLE',
+                'Time': current_time
+            }
+
+            humid = {
+                'Dimensions': dimensions,
+                'MeasureName': 'humidity',
+                'MeasureValue': str(meter.humidity),
+                'MeasureValueType': 'DOUBLE',
+                'Time': current_time
+            }
+
+            records.append(temp)
+            records.append(humid)
+
+        print('Writing records')
+        write_client.write_records(
+            database_name=aws_config['timestream_db_name'],
+            table_name=aws_config['timestream_db_table'],
+            records=records
+        )
+
+    else:
+        print('No meters found')
 
 
 if __name__ == '__main__':
@@ -70,6 +135,8 @@ if __name__ == '__main__':
         desklight()
     elif command == 'temp':
         print_meter_readings()
+    elif command == 'temp_record':
+        record_meter_readings()
     else:
         test_devices(bot_press=False)
 
--- switchbot/switchbot_devices.py
+++ switchbot/switchbot_devices.py
@@ -3,14 +3,15 @@
 
 class Meter(object):
 
-    def __init__(self, name):
+    def __init__(self, name, device_id):
         self.device_name = name
+        self.device_id = device_id
         self.temperature = 0
         self.humidity = 0
 
     def set_measurements(self, temp, humidity):
         # Temp from device status is given in C
-        self.temperature = self.convert_C_to_F(temp)
+        self.temperature = round(self.convert_C_to_F(temp), 1)
         self.humidity = humidity
 
     def convert_C_to_F(self, temp):
@@ -18,4 +19,4 @@ class Meter(object):
         return (temp * 1.8) + 32
 
     def __str__(self):
-        return f"{self.device_name}\nTemperature: {round(self.temperature, 1)} F\nHumidity: {self.humidity} %\n"
+        return f"{self.device_name}\nTemperature: {self.temperature} F\nHumidity: {self.humidity} %\n"
